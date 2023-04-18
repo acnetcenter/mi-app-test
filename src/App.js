b208dcf628f7a7bec2d78dd6cc5f57a493e4e461
kike1978 <garias@acadministracion.com>
2023-04-18 16:55:36 -0600

    repetidores

--- src/App.js
+++ src/App.js
@@ -6,7 +6,30 @@ function App() {
     <div className="App">
       <div className="contenedor-principal">
         <h1>Este es lorem itsum jugloie WEB</h1>
-        <Testimonio/>
+        <Testimonio
+          nombre="Emma"
+          pais="Suecia"
+          imagen="emma"
+          cargo="Ingeniera de Software"
+          empresa="Spotify"
+          testimonio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce cursus luctus neque sit amet laoreet. Praesent rhoncus rhoncus lectus, ac consectetur risus porttitor sed. Vivamus nunc nulla, viverra vitae quam in, volutpat interdum dui. Nullam eget ultrices est, quis finibus orci. Integer egestas, massa vel finibus congue, risus metus hendrerit orci, ac gravida diam sapien eget orci. Donec leo nisl, mollis vitae libero eget, suscipit viverra nibh. Pellentesque id posuere felis. Nulla eu ex ac eros efficitur semper non non sapien. "
+        />
+        <Testimonio
+          nombre="Sarah"
+          pais="Tilaran"
+          imagen="sarah"
+          cargo="Ingeniera de Software"
+          empresa="Spotify"
+          testimonio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce cursus luctus neque sit amet laoreet. Praesent rhoncus rhoncus lectus, ac consectetur risus porttitor sed. Vivamus nunc nulla, viverra vitae quam in, volutpat interdum dui. Nullam eget ultrices est, quis finibus orci. Integer egestas, massa vel finibus congue, risus metus hendrerit orci, ac gravida diam sapien eget orci. Donec leo nisl, mollis vitae libero eget, suscipit viverra nibh. Pellentesque id posuere felis. Nulla eu ex ac eros efficitur semper non non sapien. "
+        />
+        <Testimonio
+          nombre="Shawn"
+          pais="Palmares"
+          imagen="shawn"
+          cargo="Ingeniera de Software"
+          empresa="Spotify"
+          testimonio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce cursus luctus neque sit amet laoreet. Praesent rhoncus rhoncus lectus, ac consectetur risus porttitor sed. Vivamus nunc nulla, viverra vitae quam in, volutpat interdum dui. Nullam eget ultrices est, quis finibus orci. Integer egestas, massa vel finibus congue, risus metus hendrerit orci, ac gravida diam sapien eget orci. Donec leo nisl, mollis vitae libero eget, suscipit viverra nibh. Pellentesque id posuere felis. Nulla eu ex ac eros efficitur semper non non sapien. "
+        />
       </div>
     </div>
   );
--- src/componentes/Testimonios.js
+++ src/componentes/Testimonios.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "../styles-sheets/Testimonio.css";
 
-function Testimonio() {
+function Testimonio(props) {
   return (
     <div className="contenedor-testimonio">
       <img
         className="imagen-testimonio"
-        src={require("../imagenes/testimonio-emma.jpg")}
+        src={require(`../imagenes/testimonio-${props.imagen}.jpg`)}
         alt="Foto de Emma"/>
       <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio">Emma Bostian en Suecia</p>
-        <p className="cargo-testimonio">Ingeniera de Software en Spotify</p>
-        <p className="texto-testimonio">Aliquam odio nisl, finibus sit amet urna vel, pretium ultricies tortor. Maecenas in porttitor purus. Integer venenatis semper fringilla. Cras sollicitudin ut erat nec eleifend. Ut dui odio, feugiat vel feugiat vitae, semper in ex. Duis condimentum ac velit sed suscipit. Praesent egestas tortor sed dictum vestibulum. Curabitur commodo tristique ante non tristique. Duis finibus non sem nec laoreet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tincidunt nibh</p>
+        <p className="nombre-testimonio">{props.nombre} en {props.pais}</p>
+        <p className="cargo-testimonio">{props.cargo} en {props.empresa}</p>
+        <p className="texto-testimonio">{props.testimonio}</p>
       </div>
     </div>
   );
